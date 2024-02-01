af435dcdeaa5708c3f8a673f50d6ed3c7574621d
Kalleepar <kalleepar@gmail.com>
2024-01-31 23:22:44 -0500

    fixed port 2

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT
 
 
 const hbs = exphbs.create({ helpers: dateHelper });
